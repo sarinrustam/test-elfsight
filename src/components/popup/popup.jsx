b140d5595e71ed27eaa6dc9cc8b2a4acd86a891c
Rustam Sarin <sarin.rustam@icloud.com>
2020-10-12 10:45:01 +0300

    Исправил баг с путями

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -57,7 +57,7 @@ const Popup = (props) => (
   <PopupOverlay>
     <PopupContainer>
       <PopupClose onClick={props.handlePopupClose}>
-        <PopupCloseIcon src="/closeIcon.svg" />
+        <PopupCloseIcon src="./closeIcon.svg" />
       </PopupClose>
       <PopupContent>{props.children}</PopupContent>
     </PopupContainer>
